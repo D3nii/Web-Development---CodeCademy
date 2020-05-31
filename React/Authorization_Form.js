3721d13cbd71668510b3199d6b70c2b0ea7527e2
Muhammad Danyal Jamil Chohan <56127751+D3nii@users.noreply.github.com>
2020-05-31 16:03:49 +0300

    React Project

---
+++ React/Authorization_Form.js
@@ -0,0 +1,52 @@
+import React from 'react';
+import ReactDOM from 'react-dom';
+
+class Contact extends React.Component {
+  constructor(props) {
+    super(props);
+    this.state = {
+      password: 'swordfish',
+      authorized: false
+    };
+    this.authorize = this.authorize.bind(this);
+  }
+
+  authorize(e) {
+    const password = e.target.querySelector(
+      'input[type="password"]').value;
+    const auth = password == this.state.password;
+    this.setState({
+      authorized: auth
+    });
+  }
+
+  render() {
+    let login = (<form action="#" onSubmit={this.authorize}>
+                   <input type="password" placeholder="Password"/>
+                   <input type="submit"/>
+                 </form>);
+    const contactInfo = (
+
+    );
+
+    return (
+      <div id="authorization">
+        <h1> {this.state.authorized ? 'Contact' : 'Enter the Password'} </h1>
+        <h1> {this.state.authorized ? contactInfo : login} </h1>
+        <ul>
+          <li>
+            client@example.com
+          </li>
+          <li>
+            555.555.5555
+          </li>
+        </ul>
+      </div>
+    );
+  }
+}
+
+ReactDOM.render(
+  <Contact />, 
+  document.getElementById('app')
+);
