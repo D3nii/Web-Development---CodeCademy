3df796f82328fb8f867f9d02c51a0ff41e2f4cd3
Muhammad Danyal Jamil Chohan <56127751+D3nii@users.noreply.github.com>
2020-05-31 19:18:42 +0300

    Ravenous Part - 1

---
+++ React/ravenous/src/components/App/App.js
@@ -0,0 +1,19 @@
+import React from 'react';
+import './App.css';
+
+import BusinessList from '../BusinessList/BusinessList';
+import SearchBar from '../SearchBar/SearchBar';
+
+class App extends React.Component {
+  render() {
+    return (
+      <div className="App">
+        <h1>ravenous</h1>
+        <SearchBar />
+        <BusinessList />
+      </div>
+    );
+  }
+}
+
+export default App;
