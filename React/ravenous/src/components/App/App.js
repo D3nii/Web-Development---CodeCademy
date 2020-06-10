1402dfdaddf5eb464cf33d1310db971a5e27fbc9
Danyal Jamil <danyal.jcc@gmail.com>
2020-06-10 03:37:55 +0300

    Ravenous Part - 3

--- React/ravenous/src/components/App/App.js
+++ React/ravenous/src/components/App/App.js
@@ -20,11 +20,15 @@ let businesses = [business, business, business,
 				  business, business, business];
 
 class App extends React.Component {
+  searchYelp(term, location, sortBy) {
+    console.log(`Searching Yelp with ${term}, ${location}, ${sortBy}`);
+  }
+
   render() {
     return (
       <div className="App">
         <h1>ravenous</h1>
-        <SearchBar />
+        <SearchBar searchYelp={this.searchYelp}/>
         <BusinessList businesses={businesses}/>
       </div>
     );
